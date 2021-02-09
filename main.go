f99303d57b178f9815a21fcc00475ace2c898e25
OrlovM <orlov@email.com>
2021-02-09 23:27:59 +0300

    Bugs fixed, code quality improved, and made some optimizations

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 const (
@@ -12,34 +15,67 @@ const (
 
 // http request handler
 type handler struct {
-	// request counter
-	cnt int32
+	cnt       int32       // request counter
+	writeChan chan string // channel for `src` query param from URLs
 }
 
 func main() {
-	// remove output file before starting
-	os.Remove(out)
-
 	h := new(handler)
+	h.writeToOut()
 	err := http.ListenAndServe(":8888", h)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 }
 
-func (h handler) ServeHTTP(rsp http.ResponseWriter, req *http.Request) {
+func (h *handler) ServeHTTP(rsp http.ResponseWriter, req *http.Request) {
 	// parse `src` query param from URL
 	url := *req.URL
 	q := url.Query()
-	src := q["src"][0]
+	src := q["src"]
+	if src == nil {
+		rsp.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	go func() { h.writeChan <- src[0] }()
+}
+
+// write request counter and query param form handler.writeChan to file
+func (h *handler) writeToOut() {
+
+	sigChan := make(chan os.Signal, 1)
+	h.writeChan = make(chan string)
+
+	signal.Notify(sigChan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
 	go func() {
-		// write request counter and query param to file
-		f, _ := os.OpenFile(out, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0777)
-		f.Write([]byte(fmt.Sprintf("%d: ", h.cnt)))
-		h.cnt++
-		f.Write([]byte(src))
-		f.Write([]byte("\n"))
+		//Create file if it does not exist, or truncate if exists
+		f, err := os.Create(out)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for {
+			select {
+			case s := <-h.writeChan:
+				_, err := f.Write([]byte(fmt.Sprintf("%d: %v \n", h.cnt, s)))
+				if err != nil {
+					log.Fatal(err)
+				}
+				h.cnt++
+			case sig := <-sigChan:
+				//Close file and exit
+				err := f.Close()
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				fmt.Println(sig)
+				os.Exit(0)
+			}
+		}
 	}()
-	// respond with 200 - OK
-	rsp.WriteHeader(200)
 }
